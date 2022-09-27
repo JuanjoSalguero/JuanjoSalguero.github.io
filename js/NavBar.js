7087fca206f63d3a29d06529547d2ef87e8fdaa0
JuanjoSalguero <juanjosalguero96@hotmail.com>
2022-09-27 15:45:42 +0200

    first commit

---
+++ js/NavBar.js
@@ -0,0 +1,15 @@
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+
+/* Toggle mobile menu */
+function toggleMenu() {
+  menu.classList.toggle("active");
+  const icon = toggle.querySelector("a i.fas");
+  icon.classList.toggle('fa-bars');
+  icon.classList.toggle('fa-times');
+}
+
+/* Event Listeners */
+menu.addEventListener("click", toggleMenu, false);
+
+
