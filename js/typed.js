a841a0ee8ded1b6a3d9240095615eebb51d7b5e4
JuanjoSalguero <juanjosalguero96@hotmail.com>
2022-11-25 08:17:21 +0100

    .

--- js/typed.js
+++ js/typed.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".auto-type",{
-    strings:["Software Developer","Mobile and Desktop apps" , "Welcome to my web"],
+    strings:["Software Developer","Mobile, Web and Desktop apps" , "Welcome to my web"],
     typeSpeed: 120,
     backSpeed: 70,
     loop: true
