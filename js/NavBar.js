f7a590b6d8de00eb93b17637be9eb617ab92af42
JuanjoSalguero <juanjosalguero96@hotmail.com>
2023-01-17 23:37:38 +0100

    Desing changes

--- js/NavBar.js
+++ js/NavBar.js
@@ -3,14 +3,11 @@ const menu = document.querySelector(".menu");
 
 /* Toggle mobile menu */
 function toggleMenu() {
-  if (menu.classList.contains("active")) {
-    menu.classList.remove("active");
-    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
-  } else {
-    menu.classList.add("active");
-    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
-  }
+  menu.classList.toggle("active");
+  const icon = toggle.querySelector("a i.fas");
+  icon.classList.toggle('fa-bars');
+  icon.classList.toggle('fa-times');
 }
 
 /* Event Listeners */
-toggle.addEventListener("click", toggleMenu, false);
+menu.addEventListener("click", toggleMenu, false);
