f9bb1b38ecb77c9817ee001ac3255035d4cf317f
JuanjoSalguero <juanjosalguero96@hotmail.com>
2023-01-21 12:59:06 +0100

    Download buttons

--- js/typed.js
+++ js/typed.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".auto-type",{
-    strings:["Software Developer","Mobile, Web and Desktop apps" , "Welcome to my web"],
+    strings:["Software Developer","Mobile, Web and Desktop apps" , "Welcome to my portfolio"],
     typeSpeed: 120,
     backSpeed: 70,
     loop: true
